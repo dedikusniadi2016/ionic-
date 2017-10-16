530987f093007ca408fb5d748cdcc6b8afaf9eea
bisnis indonesia <bisnisindonesia@bisniss-iMac.local>
2017-10-16 15:27:33 +0700

    ddd

--- src/pages/jabar/jabar.module.ts
+++ src/pages/jabar/jabar.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { JabarPage } from './jabar';
 
 @NgModule({
   declarations: [
-    JabarPage,
   ],
   imports: [
-    IonicPageModule.forChild(JabarPage),
   ],
 })
 export class JabarPageModule {}
